4df6fb8dedcdbd40a8755a44b6e9316d4cec72b9
Uliana <Uliana@mail.ru>
2022-10-10 16:31:37 +0300

    dz6

---
+++ zadanie2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите значение u");
+double u = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число s");
+double s = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение t");
+double t = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число a");
+double a = Convert.ToInt32(Console.ReadLine());
+
+double x = (t + u)/(s + a);
+double y = a * x + t;
+
+Console.WriteLine($"прямые пересекутся в точке координат {x}, {y}");
